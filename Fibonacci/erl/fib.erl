ca4e5e74f8afe27d1eabb1918319eb93f76461c9
zhizhuwang <zhizhuwang@gmail.com>
2015-03-28 21:37:19 +0800

    fib with tail recursion

--- Fibonacci/erl/fib.erl
+++ Fibonacci/erl/fib.erl
@@ -1,5 +1,5 @@
 -module(fib).
--export([fib/1]).
+-export([fib/1,fib_tail_recursive/1]).
 
 
 fib(0) -> 0;
@@ -7,3 +7,12 @@ fib(1) -> 1;
 fib(N) -> 
 	fib(N-1) + fib(N-2).
 	
+
+fib_tail_recursive(0) -> 0;
+fib_tail_recursive(1) -> 1;
+fib_tail_recursive(N) -> 
+	fib_tail_recursive_internal(N, 0, 1).
+
+fib_tail_recursive_internal(1, _, B) -> B;
+fib_tail_recursive_internal(N, A, B) ->
+	fib_tail_recursive_internal(N-1, B, A+B).
