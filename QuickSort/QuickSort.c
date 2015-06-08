70a7c18dded23d9a615dbad2a8b69a898bbcf47b
zhizhuwang <zhizhuwang@gmail.com>
2015-06-08 22:00:15 +0800

    improve partition method of Quick Sort

--- QuickSort/QuickSort.c
+++ QuickSort/QuickSort.c
@@ -13,19 +13,18 @@ void QSort(int s[], int start, int end)
 	int i = start+1, j = end;
 	int pivot = s[start];
 	int t;
-	int x;
 	if(start >= end)
 	{
 		return ;
 	}
-	while(i <= j)
+	while(i < j)
 	{
-		while(s[i] <= pivot)
+		while(i<j && s[i] <= pivot)
 		{
 			s[i-1] = s[i];
 			i ++;
 		}
-		while(s[j] >= pivot)
+		while(i<j && s[j] >= pivot)
 		{
 			j --;
 		}
@@ -42,10 +41,30 @@ void QSort(int s[], int start, int end)
 
 }
 
+void QSort2(int s[], int low, int high)
+{
+	int i = low;
+	int j = high;
+	int pivot = s[low];
+	if(low >= high)
+		return;
+	while(i < j)
+	{
+		while(i<j && s[j] >= pivot) j--;
+		s[i] = s[j];
+
+		while(i<j && s[i] <= pivot) i++;
+		s[j] = s[i];
+	}
+	s[i] = pivot;
+	QSort2(s, low, i - 1);
+	QSort2(s, i+1, high);
+}
+
 void testArrayInAscendingOrder() {
 	int i;
 	int a[] = { 1, 2, 3 ,4};
-	QSort(a,0,sizeof(a) / sizeof(int));
+	QSort2(a,0,sizeof(a) / sizeof(int));
 	for (i = 0; i < sizeof(a) / sizeof(int); i++)
 		printf("%d ", a[i]);
 	printf("\n");
@@ -54,7 +73,7 @@ void testArrayInAscendingOrder() {
 void testArrayWithMultiple() {
 	int i;
 	int a[] = { 2,1,3,0,7 };
-	QSort(a, 0, sizeof(a) / sizeof(int) - 1);
+	QSort2(a, 0, sizeof(a) / sizeof(int) - 1);
 	for (i = 0; i < sizeof(a) / sizeof(int); i++)
 		printf("%d ", a[i]);
 	printf("\n");
@@ -63,7 +82,26 @@ void testArrayWithMultiple() {
 void testArrayWithMultiple2() {
 	int i;
 	int a[] = { 4,2,3,5,6,1,2 };
-	QSort(a, 0, sizeof(a) / sizeof(int) - 1);
+//	int a[] = { 4,2,3,5};
+	QSort2(a, 0, sizeof(a) / sizeof(int) - 1);
+	for (i = 0; i < sizeof(a) / sizeof(int); i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+
+void testArrayWithMultiple3() {
+	int i;
+	int a[] = { 38,27,13 };
+	QSort2(a, 0, sizeof(a) / sizeof(int) - 1);
+	for (i = 0; i < sizeof(a) / sizeof(int); i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+
+void testArrayWithMultiple4() {
+	int i;
+	int a[] = { 2,1,3,0,7 };
+	QSort2(a, 0, sizeof(a) / sizeof(int) - 1);
 	for (i = 0; i < sizeof(a) / sizeof(int); i++)
 		printf("%d ", a[i]);
 	printf("\n");
@@ -73,6 +111,8 @@ int main()
 	testArrayInAscendingOrder();
 	testArrayWithMultiple();
 	testArrayWithMultiple2();
+	testArrayWithMultiple3();
+	testArrayWithMultiple4();
 
 	printf("hello world\n");
 	return 0;
