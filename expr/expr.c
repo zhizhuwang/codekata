bb41f3f1e69bc1846488cc71519a38292d9da0d7
zhizhuwang <zhizhuwang@gmail.com>
2015-06-15 20:39:40 +0800

    sub() added

--- expr/expr.c
+++ expr/expr.c
@@ -22,19 +22,30 @@ Result num(Context * ctx)
 Result add(Context * ctx)
 {
 	int v = 0;
+	char optr;
 	Result r = num(ctx);
 
 	v = r.value;
-	while(r.ctx.str[r.ctx.pos] == '+')
+	while((optr = r.ctx.str[r.ctx.pos]) == '+' ||
+			(optr = r.ctx.str[r.ctx.pos]) == '-')
 	{
 		r.ctx.pos ++;
 		r = num(&r.ctx);
-		v += r.value;
+		if(optr == '+')
+			v += r.value;
+		if(optr == '-')
+			v -= r.value;
 	}
 	r.value = v;
 	return r;
 }
 int exprAdd(const char *s)
+{
+	Context ctx = {s, 0};
+	return add(&ctx).value;
+}
+
+int exprSub(const char *s)
 {
 	Context ctx = {s, 0};
 	return add(&ctx).value;
@@ -47,9 +58,16 @@ void test_add()
 	assert(exprAdd("1+2+3") == 1+2+3);
 	assert(exprAdd("1+2+3+4") == 1+2+3+4);
 }
+
+void test_sub()
+{
+	assert(exprSub("1-2") == 1-2);
+	assert(exprSub("1-2-3") == 1-2-3);
+}
 int main()
 {
 	test_add();
+	test_sub();
 	printf("hello world\n");
 	return 0;
 }
