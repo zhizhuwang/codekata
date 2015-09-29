e3a862651035d5e6b8a3e6c8e2958a4941b6a639
zhizhuwang <zhizhuwang@gmail.com>
2015-09-29 22:11:51 +0800

    add implement of Triangleswith generator

--- Triangles/triangles.py
+++ Triangles/triangles.py
@@ -24,6 +24,34 @@ def triangles(n):
 	return t[-1]
 
 
+def triangles_with_generator():
+	i = 0;
+	t=[]
+	while True:
+		if i == 0:
+			t = [1]
+		elif i == 1:
+			t = [1,1]
+		else:
+			t = next_line(t,i)
+			
+		yield t
+		i = i+1
+
+	return 
+
+def next_line(prev_line, i):
+	x = [1]
+	j = 1
+	while j < i:
+		x.append(prev_line[j-1] + prev_line[j])
+		j = j + 1
+
+	x.append(1)
+	
+	return x
+
+
 def main():
 	print triangles(1)
 	print triangles(2)
@@ -31,6 +59,14 @@ def main():
 	print triangles(4)
 	print triangles(5)
 	print triangles(6)
+
+	n = 0
+	for line in triangles_with_generator():
+		print line
+		n = n + 1
+		if n == 10:
+			break
+
 
 
 if __name__ == '__main__':
