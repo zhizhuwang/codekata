f1f11976bfab17b4324ddf3ea0d79b27741998c1
zhizhuwang <zhizhuwang@gmail.com>
2015-11-30 22:58:50 +0800

    extact sementics of rules

--- FizzBuzzWhizz/FizzBuzzWhizz.c
+++ FizzBuzzWhizz/FizzBuzzWhizz.c
@@ -271,9 +271,23 @@ void freeRule(struct rule* r)
 int main()
 {
 
-//	struct rule * r1_3 = atom(times_3, to_Fizz);
-//	struct rule * r1_5 = atom(times_5, to_Buzz);
-//	struct rule * r1_7 = atom(times_7, to_Whizz);
+/*
+ *  sementics of rules
+ *
+	struct rule * r1_3 = atom(times_3, to_Fizz);
+	struct rule * r1_5 = atom(times_5, to_Buzz);
+	struct rule * r1_7 = atom(times_7, to_Whizz);
+
+	struct rule * r1 = or3(r1_3, r1_5,r1_7);
+	struct rule * r2 = or4(and3(r1_3, r1_5, r1_7),
+			and(r1_3, r1_5),
+			and(r1_5, r1_7),
+			and(r1_3, r1_7));
+	struct rule * r3 = atom(contains_3, to_Fizz);
+	struct rule * rd = atom(always_true, nope);
+
+	struct rule * spec = or4(r3,r2,r1,rd);
+*/
 
 	struct rule * r1 = or3(ATOM_3, ATOM_5,ATOM_7);
 	struct rule * r2 = or4(and3(ATOM_3, ATOM_5, ATOM_7),
