60017a025ec1e958ee7495955fda726b995fb2cf
zhizhuwang <zhizhuwang@gmail.com>
2016-04-01 16:01:32 +0800

    Update learn_haskell_by_examples.hs

--- learn_haskell_by_examples.hs
+++ learn_haskell_by_examples.hs
@@ -13,3 +13,24 @@ Node 28 (Node 4 EmptyTree (Node 8 EmptyTree (Node 12 EmptyTree (Node 20 EmptyTre
 
 
 -- type是值的标签，kind是类型的标签
+
+-- query the title by id form database 
+post = Post.find_by_id(1)
+if post
+  return post.title
+else
+  return nil
+end
+-- In Haskell we can write like this: 
+fmap (getPostTitle) (findPost 1)
+-- <$> is the infix version of fmap, so you will often see this instead:
+getPostTitle <$> (findPost 1)
+
+
+-- Maybe and List are Funtors:
+instance Functor Maybe where
+    fmap (Just val) = Just (fmap val)
+    fmap (Nothing)  = Nothing
+    
+instance Functor [] where 
+   fmap = map
