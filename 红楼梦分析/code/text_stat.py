3e25f15e5aac65cddd54cf4d599b62b8a31a2f57
zhizhuwang <zhizhuwang@gmail.com>
2018-12-16 00:32:08 +0800

    1.3 completed

--- 红楼梦分析/code/text_stat.py
+++ 红楼梦分析/code/text_stat.py
@@ -27,7 +27,7 @@ def open_origin_file():
 			elif re.match(r'^第八十一回\s+.*$', line):
 				first_part = False
 				second_part = False
-				third_pard = True	
+				third_part = True	
 				f_part_2.close()
 
 			if first_part:
@@ -37,13 +37,15 @@ def open_origin_file():
 			elif third_part:
 				f_part_3.write(line)
 
+		f_part_3.close()
+
+
 def adverb_count_in_part1():
 	with open('part1.txt', 'r') as f:
 		for line in f:
 			for adverb in adverb_count_part1.keys():
 				if(re.search(r''+adverb, line)):
 					adverb_count_part1[adverb] = adverb_count_part1[adverb] + 1
-	print adverb_count_part1
 
 	with open('result.txt','a+') as f:
 		f.write("part1: ")
@@ -86,9 +88,75 @@ def adverb_count():
 
 
 
+function_words = '或亦方即皆仍故尚呀吗咧罢么呢让向往就但越再更很偏'
+function_word_count_part1 = {'或':0,'亦':0,'方':0,'即':0,'皆':0,'仍':0,'故':0,'尚':0,'呀':0,'吗':0,'咧':0,'罢':0,'么':0,'呢':0,'让':0,'向':0,'往':0,'就':0,'但':0,
+'越':0,'再':0,'更':0,'很':0,'偏':0}
+function_word_count_part2 = {'或':0,'亦':0,'方':0,'即':0,'皆':0,'仍':0,'故':0,'尚':0,'呀':0,'吗':0,'咧':0,'罢':0,'么':0,'呢':0,'让':0,'向':0,'往':0,'就':0,'但':0,
+'越':0,'再':0,'更':0,'很':0,'偏':0}
+function_word_count_part3 = {'或':0,'亦':0,'方':0,'即':0,'皆':0,'仍':0,'故':0,'尚':0,'呀':0,'吗':0,'咧':0,'罢':0,'么':0,'呢':0,'让':0,'向':0,'往':0,'就':0,'但':0,
+'越':0,'再':0,'更':0,'很':0,'偏':0}
+
+def function_word_count_in_part1():
+	total_count = 0
+	with open('part1.txt', 'r') as f:
+		for line in f:
+			text = line.decode('utf-8')
+			for i in text:
+				total_count = total_count + 1
+				if i in function_words.decode('utf-8'):
+					key = i.encode('utf-8')
+					function_word_count_part1[key] = function_word_count_part1[key] + 1
+
+	with open('result.txt','a+') as f:
+		f.write("Function word in part1: \n")
+		for (k,v) in function_word_count_part1.items():
+			f.write('{0}:{1}:{2}%\n '.format(k, str(v),str((float)(v)/(float)(total_count)*100)))
+		f.write("\n");
+
+def function_word_count_in_part2():
+	total_count = 0
+	with open('part2.txt', 'r') as f:
+		for line in f:
+			text = line.decode('utf-8')
+			for i in text:
+				total_count = total_count + 1
+				if i in function_words.decode('utf-8'):
+					key = i.encode('utf-8')
+					function_word_count_part2[key] = function_word_count_part2[key] + 1
+
+	with open('result.txt','a+') as f:
+		f.write("Function word in part2: \n")
+		for (k,v) in function_word_count_part2.items():
+			f.write('{0}:{1}:{2}%\n '.format(k, str(v),str((float)(v)/(float)(total_count)*100)))
+		f.write("\n");
+
+def function_word_count_in_part3():
+	total_count = 0
+	with open('part3.txt', 'r') as f:
+		for line in f:
+			text = line.decode('utf-8')
+			for i in text:
+				total_count = total_count + 1
+				if i in function_words.decode('utf-8'):
+					key = i.encode('utf-8')
+					function_word_count_part3[key] = function_word_count_part3[key] + 1
+
+	with open('result.txt','a+') as f:
+		f.write("Function word in part3: \n")
+		for (k,v) in function_word_count_part3.items():
+			f.write('{0}:{1}:{2}%\n '.format(k, str(v),str((float)(v)/(float)(total_count)*100)))
+		f.write("\n");
+
+def function_word_count():
+	function_word_count_in_part1()
+	function_word_count_in_part2()
+	function_word_count_in_part3()
+
 def main():
-#	open_origin_file()
-	adverb_count()
+	open_origin_file()
+#	adverb_count()
+	print len('红楼梦分析'.decode('utf-8'))
+	function_word_count()
 
 if __name__ == '__main__':
 	main()
